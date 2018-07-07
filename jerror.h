fbaf3bf05b91207ca0a8c96fabc7aa39795b8d78
KnicKnic <35521284+KnicKnic@users.noreply.github.com>
2018-07-06 23:47:14 -0700

    remove all trace routines due to mismatch function pointer types in wasm

--- jerror.h
+++ jerror.h
@@ -202,103 +202,116 @@ JMESSAGE(JWRN_TOO_MUCH_DATA, "Application transferred too many scanlines")
 #ifndef JERROR_H
 #define JERROR_H
 
+#define MAKESTMT(stuff)		do { stuff } while (0)
+#define UNREFERENCED_PARAM_JPEG(stuff)		do { (void)(stuff); } while (0)
+
+#define UNREFERENCED_PARAM_JPEG10(_s0,_s1,_s2,_s3,_s4,_s5,_s6,_s7,_s8,_s9)		do { (void)(_s0);(void)(_s1);(void)(_s2);(void)(_s3);(void)(_s4);(void)(_s5);(void)(_s6);(void)(_s7);(void)(_s8);(void)(_s9); } while (0)
+#define UNREFERENCED_PARAM_JPEG5(_s0,_s1,_s2,_s3,_s4) UNREFERENCED_PARAM_JPEG10(_s0,_s1,_s2,_s3,_s4,0,0,0,0,0)
+#define UNREFERENCED_PARAM_JPEG2(_s0,_s1) UNREFERENCED_PARAM_JPEG10(_s0,_s1,0,0,0,0,0,0,0,0)
 /* Macros to simplify using the error and trace message stuff */
 /* The first parameter is either type of cinfo pointer */
 
 /* Fatal errors (print message and exit) */
 #define ERREXIT(cinfo,code)  \
-  ((cinfo)->err->msg_code = (code), \
-   (*(cinfo)->err->error_exit) ((j_common_ptr) (cinfo)))
+  UNREFERENCED_PARAM_JPEG2(cinfo, code);
 #define ERREXIT1(cinfo,code,p1)  \
-  ((cinfo)->err->msg_code = (code), \
-   (cinfo)->err->msg_parm.i[0] = (p1), \
-   (*(cinfo)->err->error_exit) ((j_common_ptr) (cinfo)))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(p1);
 #define ERREXIT2(cinfo,code,p1,p2)  \
-  ((cinfo)->err->msg_code = (code), \
-   (cinfo)->err->msg_parm.i[0] = (p1), \
-   (cinfo)->err->msg_parm.i[1] = (p2), \
-   (*(cinfo)->err->error_exit) ((j_common_ptr) (cinfo)))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(p1);\
+  UNREFERENCED_PARAM_JPEG(p2);
 #define ERREXIT3(cinfo,code,p1,p2,p3)  \
-  ((cinfo)->err->msg_code = (code), \
-   (cinfo)->err->msg_parm.i[0] = (p1), \
-   (cinfo)->err->msg_parm.i[1] = (p2), \
-   (cinfo)->err->msg_parm.i[2] = (p3), \
-   (*(cinfo)->err->error_exit) ((j_common_ptr) (cinfo)))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(p1);\
+  UNREFERENCED_PARAM_JPEG(p2);\
+  UNREFERENCED_PARAM_JPEG(p3);
 #define ERREXIT4(cinfo,code,p1,p2,p3,p4)  \
-  ((cinfo)->err->msg_code = (code), \
-   (cinfo)->err->msg_parm.i[0] = (p1), \
-   (cinfo)->err->msg_parm.i[1] = (p2), \
-   (cinfo)->err->msg_parm.i[2] = (p3), \
-   (cinfo)->err->msg_parm.i[3] = (p4), \
-   (*(cinfo)->err->error_exit) ((j_common_ptr) (cinfo)))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(p1);\
+  UNREFERENCED_PARAM_JPEG(p2);\
+  UNREFERENCED_PARAM_JPEG(p3);\
+  UNREFERENCED_PARAM_JPEG(p4);
 #define ERREXIT6(cinfo,code,p1,p2,p3,p4,p5,p6)  \
-  ((cinfo)->err->msg_code = (code), \
-   (cinfo)->err->msg_parm.i[0] = (p1), \
-   (cinfo)->err->msg_parm.i[1] = (p2), \
-   (cinfo)->err->msg_parm.i[2] = (p3), \
-   (cinfo)->err->msg_parm.i[3] = (p4), \
-   (cinfo)->err->msg_parm.i[4] = (p5), \
-   (cinfo)->err->msg_parm.i[5] = (p6), \
-   (*(cinfo)->err->error_exit) ((j_common_ptr) (cinfo)))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(p1);\
+  UNREFERENCED_PARAM_JPEG(p2);\
+  UNREFERENCED_PARAM_JPEG(p3);\
+  UNREFERENCED_PARAM_JPEG(p4);\
+  UNREFERENCED_PARAM_JPEG(p5);\
+  UNREFERENCED_PARAM_JPEG(p6);
 #define ERREXITS(cinfo,code,str)  \
-  ((cinfo)->err->msg_code = (code), \
-   strncpy((cinfo)->err->msg_parm.s, (str), JMSG_STR_PARM_MAX), \
-   (*(cinfo)->err->error_exit) ((j_common_ptr) (cinfo)))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(str);
 
-#define MAKESTMT(stuff)		do { stuff } while (0)
 
 /* Nonfatal errors (we can keep going, but the data is probably corrupt) */
 #define WARNMS(cinfo,code)  \
-  ((cinfo)->err->msg_code = (code), \
-   (*(cinfo)->err->emit_message) ((j_common_ptr) (cinfo), -1))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(code);
 #define WARNMS1(cinfo,code,p1)  \
-  ((cinfo)->err->msg_code = (code), \
-   (cinfo)->err->msg_parm.i[0] = (p1), \
-   (*(cinfo)->err->emit_message) ((j_common_ptr) (cinfo), -1))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(p1);
 #define WARNMS2(cinfo,code,p1,p2)  \
-  ((cinfo)->err->msg_code = (code), \
-   (cinfo)->err->msg_parm.i[0] = (p1), \
-   (cinfo)->err->msg_parm.i[1] = (p2), \
-   (*(cinfo)->err->emit_message) ((j_common_ptr) (cinfo), -1))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(p1);\
+  UNREFERENCED_PARAM_JPEG(p2);
 
 /* Informational/debugging messages */
 #define TRACEMS(cinfo,lvl,code)  \
-  ((cinfo)->err->msg_code = (code), \
-   (*(cinfo)->err->emit_message) ((j_common_ptr) (cinfo), (lvl)))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(lvl);\
+  UNREFERENCED_PARAM_JPEG(code);
 #define TRACEMS1(cinfo,lvl,code,p1)  \
-  ((cinfo)->err->msg_code = (code), \
-   (cinfo)->err->msg_parm.i[0] = (p1), \
-   (*(cinfo)->err->emit_message) ((j_common_ptr) (cinfo), (lvl)))
+UNREFERENCED_PARAM_JPEG5(cinfo, lvl, code, p1, 0)
 #define TRACEMS2(cinfo,lvl,code,p1,p2)  \
-  ((cinfo)->err->msg_code = (code), \
-   (cinfo)->err->msg_parm.i[0] = (p1), \
-   (cinfo)->err->msg_parm.i[1] = (p2), \
-   (*(cinfo)->err->emit_message) ((j_common_ptr) (cinfo), (lvl)))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(lvl);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(p1);\
+  UNREFERENCED_PARAM_JPEG(p2);
 #define TRACEMS3(cinfo,lvl,code,p1,p2,p3)  \
-  MAKESTMT(int * _mp = (cinfo)->err->msg_parm.i; \
-	   _mp[0] = (p1); _mp[1] = (p2); _mp[2] = (p3); \
-	   (cinfo)->err->msg_code = (code); \
-	   (*(cinfo)->err->emit_message) ((j_common_ptr) (cinfo), (lvl)); )
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(lvl);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(p1);\
+  UNREFERENCED_PARAM_JPEG(p2);\
+  UNREFERENCED_PARAM_JPEG(p3);
 #define TRACEMS4(cinfo,lvl,code,p1,p2,p3,p4)  \
-  MAKESTMT(int * _mp = (cinfo)->err->msg_parm.i; \
-	   _mp[0] = (p1); _mp[1] = (p2); _mp[2] = (p3); _mp[3] = (p4); \
-	   (cinfo)->err->msg_code = (code); \
-	   (*(cinfo)->err->emit_message) ((j_common_ptr) (cinfo), (lvl)); )
+UNREFERENCED_PARAM_JPEG10(cinfo, lvl, code, p1, p2, p3, p4, 0, 0, 0)
 #define TRACEMS5(cinfo,lvl,code,p1,p2,p3,p4,p5)  \
-  MAKESTMT(int * _mp = (cinfo)->err->msg_parm.i; \
-	   _mp[0] = (p1); _mp[1] = (p2); _mp[2] = (p3); _mp[3] = (p4); \
-	   _mp[4] = (p5); \
-	   (cinfo)->err->msg_code = (code); \
-	   (*(cinfo)->err->emit_message) ((j_common_ptr) (cinfo), (lvl)); )
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(lvl);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(p1);\
+  UNREFERENCED_PARAM_JPEG(p2);\
+  UNREFERENCED_PARAM_JPEG(p3);\
+  UNREFERENCED_PARAM_JPEG(p4);\
+  UNREFERENCED_PARAM_JPEG(p5);
 #define TRACEMS8(cinfo,lvl,code,p1,p2,p3,p4,p5,p6,p7,p8)  \
-  MAKESTMT(int * _mp = (cinfo)->err->msg_parm.i; \
-	   _mp[0] = (p1); _mp[1] = (p2); _mp[2] = (p3); _mp[3] = (p4); \
-	   _mp[4] = (p5); _mp[5] = (p6); _mp[6] = (p7); _mp[7] = (p8); \
-	   (cinfo)->err->msg_code = (code); \
-	   (*(cinfo)->err->emit_message) ((j_common_ptr) (cinfo), (lvl)); )
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(lvl);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(p1);\
+  UNREFERENCED_PARAM_JPEG(p2);\
+  UNREFERENCED_PARAM_JPEG(p3);\
+  UNREFERENCED_PARAM_JPEG(p4);\
+  UNREFERENCED_PARAM_JPEG(p5);\
+  UNREFERENCED_PARAM_JPEG(p6);\
+  UNREFERENCED_PARAM_JPEG(p7);\
+  UNREFERENCED_PARAM_JPEG(p8);
 #define TRACEMSS(cinfo,lvl,code,str)  \
-  ((cinfo)->err->msg_code = (code), \
-   strncpy((cinfo)->err->msg_parm.s, (str), JMSG_STR_PARM_MAX), \
-   (*(cinfo)->err->emit_message) ((j_common_ptr) (cinfo), (lvl)))
+  UNREFERENCED_PARAM_JPEG(cinfo);\
+  UNREFERENCED_PARAM_JPEG(lvl);\
+  UNREFERENCED_PARAM_JPEG(code);\
+  UNREFERENCED_PARAM_JPEG(str);
 
 #endif /* JERROR_H */
